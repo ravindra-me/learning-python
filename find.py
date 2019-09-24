2a95181f2b2c3fad901060e16a56c053bd8351ac
ravindra-singh94 <ravindrarajpoot9628172@gmail.com>
2019-09-24 12:09:09 -0700

    find

---
+++ find.py
@@ -0,0 +1,4 @@
+arr=[0, 1, 2, 3, 5, 8, 13]
+def take(arr,n):
+    return(arr[0:n])
+print(take(arr,3))
--- sum.py
+++ sum.py
@@ -1,5 +1,7 @@
 letters=['a', 'b', 'c']
-def add_letters(*letters):
-    a=list(*letters)
-    return(str(chr(ord(a[-1])+1)))
+def add_letters(letters):
+    # a=list(*letters)
+    # return(str(chr(ord(a[-1])+1)))
+    letters.append(chr(ord(letters[-1])+1))
+    return letters
 print(add_letters(letters))
