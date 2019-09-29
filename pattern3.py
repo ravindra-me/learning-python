5e8a295180e6e3dfd090ff1459f30e7bf5ce37d2
ravindra-singh94 <ravindrarajpoot9628172@gmail.com>
2019-09-29 08:13:36 -0700

    c shap pattern

--- pattern3.py
+++ pattern3.py
@@ -1,6 +1,6 @@
 for row in range (7):
     for cal in range (5):
-        if (cal==0 or cal==4)or (row==0 or row ==3 or row==6)and (cal>0 and cal<4):
+        if (row==0 or row==6)or(cal==0):
             print("*",end="")
         else:
             print(end=" ")
