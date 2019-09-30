6c3c6872cf14eb6591e1244f83de66054df5c8c3
ravindra-singh94 <ravindrarajpoot9628172@gmail.com>
2019-09-30 11:18:46 -0700

    printing number in square shape

--- pattern3.py
+++ pattern3.py
@@ -1,9 +1,27 @@
 num=int(input("enter the row value"))
-for i in range (1,num+1):
-    for j in range (1,num-i+1):
-        print(" ",end=" ")
-    for j in range(i,0,-1):
-        print(j,end=" ")
-    for j in range(2,i+1):
-        print(j,end=" ")
+n_list=[[0 for x in range(num)] for y in range(num)]
+n=1
+low=0
+high=num-1
+count=int((num+1)/2)
+for i in range (count):
+    for j in range (low,high+1):
+        n_list[low][j]=n
+        n+=1
+    for j in range(low+1,high+1):
+        n_list[j][high]=n
+        n+=1
+    for j in range(high-1,low-1,-1):
+        n_list[high][j]=n
+        n+=1
+    for j in range(high-1,low,-1):
+        n_list[j][low]=n
+        n+=1
+    low=low+1
+    high=high-1
+for i in range(num):
+    for j in range(num):
+        print(n_list[i][j],end="\t")
     print()
+        
+  
