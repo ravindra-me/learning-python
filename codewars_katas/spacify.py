e441f21d0c6eb00dcfe253200cc9255cdad9a7b3
ravindra-singh94 <ravindrarajpoot9628172@gmail.com>
2019-10-04 22:04:30 +0530

    diogogal sum

--- codewars_katas/spacify.py
+++ codewars_katas/spacify.py
@@ -1,9 +1,25 @@
-string = 'hello world'
-def sapcify(string):
-    result = ""
-    for i in string:
-        result += i+" "
-    return result.rstrip()
+def printDiagonalSums(mat, n): 
+  
+    principal = 0
+    secondary = 0; 
+    for i in range(0, n):  
+        for j in range(0, n):  
+  
+             
+            if (i == j): 
+                principal += mat[i][j] 
+  
+            
+            if ((i + j) == (n - 1)): 
+                secondary += mat[i][j] 
+          
+    print("Principal Diagonal:", principal) 
+    print("Secondary Diagonal:", secondary) 
+  
 
+a = [[ 1, 2, 3, 4 ], 
+     [ 5, 6, 7, 8 ],  
+     [ 1, 2, 3, 4 ], 
+      [ 5, 6, 7, 8 ]] 
+printDiagonalSums(a, 4) 
 
-print(sapcify(string))
