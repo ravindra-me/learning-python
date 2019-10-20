a11d8c00fa4eb284619f692d18e53169577aeff5
ravindra-singh94 <ravindrarajpoot9628172@gmail.com>
2019-10-20 22:56:57 +0530

    Is my friend cheating?

---
+++ codewars_katas/ravi.html/cheating.py
@@ -0,0 +1,10 @@
+n=26
+def removNb(n):
+    total = n*(n+1)/2
+    sol = []
+    for a in range(1,n+1):
+        b = (total-a)/(a+1.0)
+        if b.is_integer() and b <= n:
+            sol.append((a,int(b)))
+    return sol
+print(removNb(n))
